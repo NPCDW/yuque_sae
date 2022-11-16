2e74263db0c768aded9adacd48ee63111ef24741
NPCDW <chenyongli0520@gmail.com>
2022-11-16 21:23:08 +0800

    格式化

--- src/format/format.rs
+++ src/format/format.rs
@@ -22,13 +22,14 @@ pub fn traversal_file(path: &Path) {
 }
 
 pub fn format_md(path: &Path) {
+    println!("格式化文档：{}", path.display());
     // 隐藏代码块和行内代码
     let map = code_hide(path);
     if crate::CONFIG.format.newline_character_convert {
-        
+        newline_character_convert(path);
     }
     if crate::CONFIG.format.clear_html_tag {
-        
+        clear_html_tag(path);
     }
     if crate::CONFIG.format.resolve_img {
         resolve_img(path);
@@ -46,7 +47,7 @@ pub fn code_hide(path: &Path) -> HashMap<String, String> {
         let code = caps.get(1).unwrap().as_str();
         // println!("code: {}", code);
         let from = format!("```{}```", code);
-        let to = format!("<<<<<<<<<<<{}>>>>>>>>>>>", count);
+        let to = format!("^^^^^^^^^^^{}^^^^^^^^^^^", count);
         copy = copy.replace(&from, &to);
         map.insert(to, from);
         count += 1;
@@ -57,7 +58,7 @@ pub fn code_hide(path: &Path) -> HashMap<String, String> {
         let code = caps.get(1).unwrap().as_str();
         // println!("inline-code: {}", code);
         let from = format!("`{}`", code);
-        let to = format!("<<<<<<<<<<<{}>>>>>>>>>>>", count);
+        let to = format!("^^^^^^^^^^^{}^^^^^^^^^^^", count);
         copy = copy.replace(&from, &to);
         map.insert(to, from);
         count += 1;
@@ -74,12 +75,32 @@ pub fn code_show(path: &Path, map: HashMap<String, String>) {
     file_util::write_file(path, &text)
 }
 
+pub fn newline_character_convert(path: &Path) {
+    let mut text = file_util::read_file(path).unwrap();
+    text = text.replace("<br />", "  \n\n");
+    file_util::write_file(path, &text);
+}
+
+pub fn clear_html_tag(path: &Path) {
+    let text = file_util::read_file(path).unwrap();
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"<[^>]+>").unwrap();
+    }
+    let text = RE.replace_all(&text, "");
+    file_util::write_file(path, &text);
+}
+
 pub fn resolve_img(path: &Path) {
     let text = file_util::read_file(path).unwrap();
     let mut copy = text.clone();
-    let re = Regex::new(r"!\[(.*?)\]\((.*?)\)").unwrap();
-    for caps in re.captures_iter(&text) {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"!\[(.*?)\]\((.*?)\)").unwrap();
+    }
+    for caps in RE.captures_iter(&text) {
         let url = caps.get(2).unwrap().as_str();
+        if !url.starts_with("http") {
+            continue;
+        }
         let name = *url.split("/").collect::<Vec<&str>>().last().unwrap().split("#").collect::<Vec<&str>>().first().unwrap();
         // println!("name: {}, url: {}", name, url);
         let filepath = path.parent().unwrap().join(&crate::CONFIG.format.resolve_img_path).join(name);
@@ -97,7 +118,7 @@ mod format_test {
 
     #[test]
     fn format_test() {
-        format(Path::new(r"D:\Temp\outline"));
+        format(Path::new(r"D:\Temp\新建文件夹\新建文件夹\5rwl6kv5pah5qgj.md"));
     }
     
     #[test]
