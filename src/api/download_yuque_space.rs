6e38d3828d482ac1b07389fd0309593916c01289
NPCDW <chenyongli0520@gmail.com>
2023-01-17 19:54:10 +0800

    导出支持语雀空间

--- src/api/download_yuque_space.rs
+++ src/api/download_yuque_space.rs
@@ -1,17 +1,15 @@
 use std::path::Path;
-use substring::Substring;
 
 use super::space_api;
 use crate::util::file_util;
 
 pub fn download(source_path: &Path) {
-    if crate::CONFIG.download.person.enable == false {
+    if crate::CONFIG.download.space.enable == false {
         println!("语雀空间版导出功能未启用");
         return;
     }
 
     println!("语雀空间版导出...");
-    file_util::create_dir(crate::CONFIG.download.space.domain.as_str().substring(2,5));
 
     println!("获取空间知识库...");
     let repos_data = space_api::get_books().unwrap_or_else(|e| {
@@ -30,14 +28,20 @@ pub fn download(source_path: &Path) {
             panic!("获取知识库文档列表失败：{:?}", e);
         });
         for doc in docs_data.data {
+            if doc.type_.as_str() != "Doc" {
+                println!("跳过文档 > {}", doc.title);
+                continue;
+            }
             println!("导出文档 > {}", doc.title);
-            let doc = api::get_doc(repos.id, doc.id).unwrap_or_else(|e| {
-                panic!("获取文档详情失败：{:?}", e);
+            let doc_export = space_api::export_doc(doc.id).unwrap_or_else(|e| {
+                panic!("导出文档失败：{:?}", e);
             });
-            let doc = doc.data;
+            let body = space_api::get_doc(doc_export.data.url).unwrap_or_else(|e| {
+                panic!("获取文档数据失败：{:?}", e);
+            });
 
-            let doc_path = user_repos_dir.join(doc.title.replace("/", "-").replace("\\", "-").replace(":", "-").replace("*", "-").replace("?", "-").replace("\"", "-").replace("<", "-").replace(">", "-").replace("|", "-") + ".md");
-            file_util::write_file(&doc_path, &doc.body);
+            let doc_path = user_repos_dir.join(format_filename(doc.title) + ".md");
+            file_util::write_file(&doc_path, &body);
         }
     }
 }
